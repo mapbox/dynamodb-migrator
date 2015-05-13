9e002b9db80ad903d45f634caa337145040b5f4c
Ryan Clark <ryan.clark.j@gmail.com>
2015-05-13 15:59:47 -0700

    test for string input to aggregator

--- test/aggregator.test.js
+++ test/aggregator.test.js
@@ -30,3 +30,30 @@ test('[aggregator] objects', function(assert) {
 
   aggregator.end();
 });
+
+test('[aggregator] strings', function(assert) {
+  var aggregator = Aggregator(1, false);
+  var count = 0;
+
+  aggregator.on('data', function(data) {
+    assert.ok(data.length <= 10, 'aggregates into blocks of up to 10x concurrency');
+    count += data.length;
+
+    data.forEach(function(d) {
+      assert.ok(Buffer.isBuffer(d.data), 'decodes base64 strings');
+    });
+  });
+
+  aggregator.on('end', function() {
+    assert.equal(count, 30, 'passed all records through');
+    assert.end();
+  });
+
+  _.range(30).forEach(function(i) {
+    aggregator.write(JSON.stringify({
+      data: 'base64:' + (new Buffer(i.toString())).toString('base64')
+    }));
+  });
+
+  aggregator.end();
+});
