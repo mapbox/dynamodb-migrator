02be3b106dc945a3fe511b8c6df92210ce80eddd
Ryan Clark <ryan.clark.j@gmail.com>
2015-06-24 15:37:17 -0600

    log to learn about how fast we're going

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Migrator = require('./lib/migrator');
 var Dyno = require('dyno');
 var split = require('split');
 var Readable = require('stream').Readable;
+var util = require('util');
 
 module.exports = function(method, database, migrate, stream, live, plainJSON, concurrency, callback) {
   require('http').globalAgent.maxSockets = 5 * concurrency;
@@ -44,6 +45,27 @@ module.exports = function(method, database, migrate, stream, live, plainJSON, co
     if (method === 'stream') return process.stdin.pipe(split());
     if (method instanceof Readable) return method.pipe(split());
   })();
+
+  scanner.scans = 0;
+  scanner.on('dbrequest', function() {
+    scanner.scans++;
+  });
+
+  var starttime = Date.now();
+  setInterval(function() {
+    var msg = util.format(
+      '\r\033[KScanner scans: %s, read depth: %s, Aggregator write depth: %s, read depth: %s, item: %s | Migrator depth: %s, active: %s, %s/s',
+      scanner.scans,
+      scanner._readableState.buffer.length,
+      aggregator.items(),
+      aggregator._writableState.buffer.length,
+      aggregator._readableState.buffer.length,
+      migrator._writableState.buffer.length,
+      migrator.active,
+      (migrator.total / ((Date.now() - starttime) / 1000)).toFixed(4)
+    );
+    process.stdout.write(msg);
+  }, 500).unref();
 
   scanner
     .pipe(aggregator)
